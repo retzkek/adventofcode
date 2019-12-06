d73e22a1c173cccc3b6bf7dfa41df97a90eb7719
Kevin Retzke <kretzke@fnal.gov>
2019-12-05 22:44:38 -0600

    implement opcodes 5-8

--- 2019/day5-sunny.rkt
+++ 2019/day5-sunny.rkt
@@ -45,9 +45,20 @@
   (exe (sub-at prog (second args) (read)) (+ step 2)))
 
 (define (op-output prog args step modes)
-  (let* ([val (param (first modes) (second args) prog)])
-    (writeln val)
-    (exe prog (+ step 2))))
+  (writeln (param (first modes) (second args) prog))
+  (exe prog (+ step 2)))
+
+(define (op-jump f prog args step modes)
+  (exe prog (if (f (param (first modes) (second args) prog))
+                (param (second modes) (third args) prog)
+                (+ step 3))))
+
+(define (op-cmp f prog args step modes)
+  (let* ([a (param (first modes) (second args) prog)]
+         [b (param (second modes) (third args) prog)]
+         [val (if (f a b) 1 0)]
+         [dest (fourth args)])
+    (exe (sub-at prog dest val) (+ step 4))))
 
 (define (exe prog step)
   (let* ([args (drop prog step)]
@@ -60,7 +71,11 @@
       [(eq? opcode 1) (op2 + prog args step modes)]
       [(eq? opcode 2) (op2 * prog args step modes)]
       [(eq? opcode 3) (op-input prog args step modes)]
-      [(eq? opcode 4) (op-output prog args step modes)])))
+      [(eq? opcode 4) (op-output prog args step modes)]
+      [(eq? opcode 5) (op-jump (λ (x) (not (eq? x 0))) prog args step modes)]
+      [(eq? opcode 6) (op-jump (λ (x) (eq? x 0)) prog args step modes)]
+      [(eq? opcode 7) (op-cmp < prog args step modes)]
+      [(eq? opcode 8) (op-cmp = prog args step modes)])))
 
 (module+ test
   (check-equal? (exe '(1 4 5 4 11 88) 0) '(1 4 5 4 99 88))
@@ -79,7 +94,7 @@
 (module+ test
   (check-equal? (read-program (open-input-string "1,2,3")) '(1 2 3)))
 
-(define-syntax-rule (check-program prog newprog)
+(define (check-program prog newprog)
   (check-equal?
    (exe (read-program (open-input-string prog)) 0)
    (read-program (open-input-string newprog))))
@@ -89,6 +104,33 @@
   (check-program "2,3,0,3,99" "2,3,0,6,99")
   (check-program "2,4,4,5,99,0" "2,4,4,5,99,9801")
   (check-program "1,1,1,4,99,5,6,0,99" "30,1,1,4,2,5,6,0,99"))
+
+(define (check-program-io program stdin stdout)
+  (let ([out (open-output-string)])
+    (parameterize ([current-input-port (open-input-string stdin)]
+                   [current-output-port out])
+      (exe (read-program (open-input-string program)) 0)
+      (check-equal? (get-output-string out) stdout))))
+
+(module+ test
+  (check-program-io "3,9,8,9,10,9,4,9,99,-1,8" "8" "> 1\n")
+  (check-program-io "3,9,8,9,10,9,4,9,99,-1,8" "2" "> 0\n")
+  (check-program-io "3,9,7,9,10,9,4,9,99,-1,8" "2" "> 1\n")
+  (check-program-io "3,9,7,9,10,9,4,9,99,-1,8" "10" "> 0\n")
+  (check-program-io "3,3,1108,-1,8,3,4,3,99" "8" "> 1\n")
+  (check-program-io "3,3,1108,-1,8,3,4,3,99" "2" "> 0\n")
+  (check-program-io "3,3,1107,-1,8,3,4,3,99" "2" "> 1\n")
+  (check-program-io "3,3,1107,-1,8,3,4,3,99" "10" "> 0\n")
+  (check-program-io "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9" "0" "> 0\n")
+  (check-program-io "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9" "22" "> 1\n")
+  (check-program-io "3,3,1105,-1,9,1101,0,0,12,4,12,99,1" "0" "> 0\n")
+  (check-program-io "3,3,1105,-1,9,1101,0,0,12,4,12,99,1" "22" "> 1\n")
+  (check-program-io "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
+                    "2" "> 999\n")
+  (check-program-io "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
+                    "8" "> 1000\n")
+  (check-program-io "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
+                    "9" "> 1001\n"))
 
 (module+ main
   (display
