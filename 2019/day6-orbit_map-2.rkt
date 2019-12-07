130669e0887ae7e8596c2fef4a5f20382f126db1
Kevin Retzke <kretzke@fnal.gov>
2019-12-07 13:40:58 -0600

    finish day6 part2

--- 2019/day6-orbit_map-2.rkt
+++ 2019/day6-orbit_map-2.rkt
@@ -2,6 +2,8 @@
 
 (require rackunit)
 
+
+
 (define (read-orbit-map ip [om '()])
   (let* ([l (read-line ip)])
     ;(displayln l)
@@ -9,18 +11,8 @@
         om
         (read-orbit-map ip (cons (string-split l ")") om)))))
 
-(define (count-orbits obj om)
-  (let ([parent (filter (λ (x) (equal? (second x) obj)) om)])
-    ;(displayln (list obj parent))
-    (if (empty? parent)
-       0
-       (+ 1 (count-orbits (first (first parent)) om)))))
-
-(define (sum-orbits om)
-  (foldl + 0 (map (λ (x) (count-orbits (second x) om)) om)))
-
-(module+ test
-  (check-eq? (let ([om (read-orbit-map (open-input-string "COM)B
+(define *test-orbit-map*
+  (read-orbit-map (open-input-string "COM)B
 B)C
 C)D
 D)E
@@ -30,15 +22,70 @@ G)H
 D)I
 E)J
 J)K
-K)L"))])
-               (sum-orbits om))
-             42))
+K)L
+K)YOU
+I)SAN")))
+
+(module+ test
+  (check-equal? *test-orbit-map*
+                '(("I" "SAN")
+                  ("K" "YOU")
+                  ("K" "L")
+                  ("J" "K")
+                  ("E" "J")
+                  ("D" "I")
+                  ("G" "H")
+                  ("B" "G")
+                  ("E" "F")
+                  ("D" "E")
+                  ("C" "D")
+                  ("B" "C")
+                  ("COM" "B"))))
+
+(define (list-orbits obj om)
+  (let ([parent (filter (λ (x) (equal? (second x) obj)) om)])
+    ;(displayln (list obj parent))
+    (if (empty? parent)
+        '()
+        (cons (first (first parent))
+              (list-orbits (first (first parent)) om)))))
+
+(module+ test
+  (check-equal? (list-orbits "J" *test-orbit-map*)
+                '("E" "D" "C" "B" "COM")))
+
+(define (count-orbits obj om)
+  (length (list-orbits obj om)))
+
+(module+ test
+  (check-equal? (count-orbits "J" *test-orbit-map*) 5))
+
+(define (sum-orbits om)
+  (foldl + 0 (map (λ (x) (count-orbits (second x) om)) om)))
+
+(module+ test
+  (check-eq? (sum-orbits *test-orbit-map*) 54))
+
+(define (transfers from to om)
+  (let ([from-orbits (list-orbits from om)]
+        [to-orbits (list-orbits to om)])
+    (apply
+     min
+     (filter-map
+      (λ (x)
+        (let ([a (index-of from-orbits x)]
+              [b (index-of to-orbits x)])
+          (if b (+ a b) #f)))
+      from-orbits))))
+
+(module+ test
+  (check-eq? (transfers "YOU" "SAN" *test-orbit-map*) 4))
 
 (module+ main
-  (sum-orbits
-   (read-orbit-map
+  (let ([om (read-orbit-map
     (open-input-file
      (command-line
       #:program "orbit-map"
       #:args (filename)
-      filename)))))
+      filename)))])
+    (displayln (transfers "YOU" "SAN" om))))
