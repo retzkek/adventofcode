aee5caa00ff2e23a50205282c8975c305e5824f5
Kevin Retzke <retzkek@gmail.com>
2022-12-02 09:16:42 -0500

    2022.2.2 april

--- 2022/day02.lisp
+++ 2022/day02.lisp
@@ -9,7 +9,7 @@ C Z")
 ;; B 0 3 6
 ;; C 6 0 3
 ;;
-(defun apl (lines)
+(defun part1 (lines)
   (april-f (with (:state
                   :in ((in (coerce lines 'vector)))
                   :out (ans1))) "
@@ -24,7 +24,27 @@ r + l r ⌷ scores
 ans1 ← +/ score ¨ in
 "))
 
-(apl (with-input-from-string (in *example1*)
-       (read-lines in)))
+(part1 (with-input-from-string (in *example1*)
+         (read-lines in)))
 
-(apl (input-as-lines 2022 2))
+(part1 (input-as-lines 2022 2))
+
+(defun part2 (lines)
+  (april-f (with (:state
+                  :in ((in (coerce lines 'vector)))
+                  :out (ans2))) "
+scores ← 3 3 ⍴ 3 6 0 0 3 6 6 0 3
+lefts ← 'ABC'
+rights ← 'X..Y..Z'
+score ← {
+l ← lefts ⍳ (⊃ ⍵)            ⍝ opponent's play
+p ← (rights ⍳ (3 ⊃ ⍵)) - 1   ⍝ how many points we need
+p + (l ⌷ scores) ⍳ p         ⍝ find the index (value) for our play, plus points we need
+}
+ans2 ← +/ score ¨ in
+"))
+
+(part2 (with-input-from-string (in *example1*)
+         (read-lines in)))
+
+(part2 (input-as-lines 2022 2))
