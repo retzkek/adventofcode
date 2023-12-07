d7725c7a2cad1c4758124be680068c389a3080cf
Kevin Retzke <retzkek@gmail.com>
2023-12-07 12:43:58 -0500

    2023.7.2 elixir

--- elixir/lib/year23/day07.ex
+++ elixir/lib/year23/day07.ex
@@ -44,7 +44,7 @@ defmodule AOC.Year23.Day07 do
     |> Enum.sort()
   end
 
-  def score_hand(h) do
+  def score_hand(h, wild \\ nil) do
     s = sort_hand(h)
 
     cond do
@@ -56,21 +56,73 @@ defmodule AOC.Year23.Day07 do
       s == [1, 1, 1, 2] -> :one_pair
       true -> :high_card
     end
+    |> then(fn score ->
+      if wild do
+        nwild = Enum.count(h, fn x -> x == wild end)
+
+        case nwild do
+          0 ->
+            score
+
+          1 ->
+            case score do
+              :four_kind -> :five_kind
+              :three_kind -> :four_kind
+              :two_pair -> :full_house
+              :one_pair -> :three_kind
+              :high_card -> :one_pair
+            end
+
+          2 ->
+            case score do
+              :full_house -> :five_kind
+              :three_kind -> :five_kind
+              :two_pair -> :four_kind
+              :one_pair -> :three_kind
+            end
+
+          3 ->
+            case score do
+              :full_house -> :five_kind
+              :three_kind -> :four_kind
+            end
+
+          _ ->
+            :five_kind
+        end
+      else
+        score
+      end
+    end)
   end
 
-  def group_hands(hands) do
-    Enum.group_by(hands, fn {h, _} -> score_hand(h) end)
+  def group_hands(hands, wild \\ nil) do
+    Enum.group_by(hands, fn {h, _} -> score_hand(h, wild) end)
   end
 
   @hand_rank [:high_card, :one_pair, :two_pair, :three_kind, :full_house, :four_kind, :five_kind]
 
-  def order_hands(hands) do
-    groups = group_hands(hands)
+  def order_hands(hands, wild \\ nil) do
+    groups = group_hands(hands, wild)
 
     Enum.flat_map(@hand_rank, fn rank ->
       (groups[rank] || [])
       |> Enum.sort(fn {a, _}, {b, _} ->
         Enum.zip(a, b)
+        |> Enum.map(fn {x, y} ->
+          {
+            if x == wild do
+              1
+            else
+              x
+            end,
+            if y == wild do
+              1
+            else
+              y
+            end
+          }
+        end)
         |> Enum.reduce_while(false, fn {x, y}, _ ->
           cond do
             x < y -> {:halt, true}
@@ -83,8 +135,8 @@ defmodule AOC.Year23.Day07 do
     end)
   end
 
-  def winnings(hands) do
-    order_hands(hands)
+  def winnings(hands, wild \\ nil) do
+    order_hands(hands, wild)
     |> Stream.with_index()
     |> Stream.map(fn {bid, i} -> bid * (i + 1) end)
     # |> Enum.to_list()
@@ -96,6 +148,13 @@ defmodule AOC.Year23.Day07 do
     AOC.input(2023, 7)
     |> read_hands()
     |> winnings()
+    |> IO.puts()
+  end
+
+  def part2() do
+    AOC.input(2023, 7)
+    |> read_hands()
+    |> winnings(11)
     |> IO.puts()
   end
 end
--- elixir/test/year23/day07_test.exs
+++ elixir/test/year23/day07_test.exs
@@ -28,6 +28,12 @@ QQQJA 483"
     assert score_hand([3, 3, 2, 2, 4]) == :two_pair
     assert score_hand([3, 3, 2, 4, 5]) == :one_pair
     assert score_hand([2, 3, 4, 5, 6]) == :high_card
+
+    assert score_hand([3, 2, 10, 3, 13], 11) == :one_pair
+    assert score_hand([10, 5, 5, 11, 5], 11) == :four_kind
+    assert score_hand([13, 13, 6, 7, 7], 11) == :two_pair
+    assert score_hand([13, 10, 11, 11, 10], 11) == :four_kind
+    assert score_hand([12, 12, 12, 11, 14], 11) == :four_kind
   end
 
   test "group hands" do
@@ -48,9 +54,11 @@ QQQJA 483"
 
   test "order hands" do
     assert order_hands(read_hands(@test_input)) == [765, 220, 28, 684, 483]
+    assert order_hands(read_hands(@test_input), 11) == [765, 28, 684, 483, 220]
   end
 
   test "winnings" do
     assert winnings(read_hands(@test_input)) == 6440
+    assert winnings(read_hands(@test_input), 11) == 5905
   end
 end
